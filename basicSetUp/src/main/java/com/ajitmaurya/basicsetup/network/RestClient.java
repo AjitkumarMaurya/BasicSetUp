a4fb70dfd4dc83d5f21093da9f6b959a0c1e6232
AJIT M MAURYA <ajitmauray@gmail.com>
2022-11-16 17:12:26 +0530

    new v10

--- basicSetUp/src/main/java/com/ajitmaurya/basicsetup/network/RestClient.java
+++ basicSetUp/src/main/java/com/ajitmaurya/basicsetup/network/RestClient.java
@@ -6,6 +6,7 @@ import com.google.gson.GsonBuilder;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RestClient {
 
@@ -18,7 +19,7 @@ public class RestClient {
 
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(baseUrl)
-                .client(UnsafeOkHttpClient.getUnsafeOkHttpClient(key,key,key))
+                .client(UnsafeOkHttpClient.getUnsafeOkHttpClient(key, key, key))
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
 
@@ -36,8 +37,20 @@ public class RestClient {
 
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl(baseUrl)
-                .client(UnsafeOkHttpClient.getUnsafeOkHttpClient(key,baseAuthUserName,baseAuthPassword))
+                .client(UnsafeOkHttpClient.getUnsafeOkHttpClient(key, baseAuthUserName, baseAuthPassword))
                 .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
+
+        Retrofit retrofit = builder.build();
+        return retrofit.create(serviceClass);
+    }
+
+    public static <S> S createServiceBody(Class<S> serviceClass, String baseUrl, String key, String baseAuthUserName, String baseAuthPassword) {
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(UnsafeOkHttpClient.getUnsafeOkHttpClient(key, baseAuthUserName, baseAuthPassword))
+                .addConverterFactory(ScalarsConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
 
         Retrofit retrofit = builder.build();
